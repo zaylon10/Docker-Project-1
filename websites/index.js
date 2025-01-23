c7ec85c4029365c5cb8131893ebaed6b12312592
Zaylon Freeman <zfreem5@wgu.edu>
2025-01-23 12:28:40 -0800

    Fix: Update JavaScript to match corresponding HTML elements and functionality

--- websites/index.js
+++ websites/index.js
@@ -1,5 +1,23 @@
-let username;
-document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("mytext").value;
-    console.log(username);
+// The JavaScript code in index.js is linked to and controls the behavior of the index.html document.
+// constants hold references to specific HTML elements with the corresponding IDs
+const decreaseBtn = document.getElementById("decreaseBtn")
+const resetBtn = document.getElementById("resetBtn")
+const increaseBtn = document.getElementById("increaseBtn")
+const countLabel = document.getElementById("countLabel")
+let count =0;
+// add event listeners (onclick) and update the count label when the respective button is clicked.
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    let count = 0;
+    countLabel.textContent = count;
 }
